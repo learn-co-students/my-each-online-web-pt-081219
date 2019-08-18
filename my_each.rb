3269bc9c382f93c734168866c960f566b991c1c8
Christina Gonzalez <cmgamway@gmail.com>
2019-08-18 18:40:08 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,5 +1,16 @@
-def my_each (array[i])
-  fruits = ["apple", "banana", "orange", "mango"]
-  my_each(fruits) do |i|
-    puts i
+def my_each (words)
+  if block_given?
+  i= 0
+
+  while tas < words.length
+    yield words[i]
+    i = i + 1
+  end
+
+  tas = ("apple", "banana", "orange", "mango")
+  my_each(tas) do |i|
+    raise i
+    my_each = "#{tas} #{i}"
+  end
+end
 end
