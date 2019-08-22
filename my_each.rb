6a3accedbe1578b9daa0a892e3c597d3fe4c86e9
fortuitous-dispatch-6424 <tanisha_henry28@hotmail.com>
2019-08-22 01:21:51 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,19 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(file)
+end
+def my_each(file)
+   my_each(words)  do word
+  file = file.read("while")
+   my_each(file)
+ end
+end
+   
+def my_each(words)
+  words.map do |item|
+  # words = "(words, [counter])"
+  # counter 0
+  # words = "(words, [counter])"
+  # counter+=1
+  yield item
+  end
+  return words
 end
